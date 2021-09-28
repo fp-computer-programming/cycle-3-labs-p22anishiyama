bc9a6be9df24d6ef73a3a56d64f32392dc2dc185
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-09-28 10:04:06 -0400

    add lab 2-1 and 2-2

---
+++ lab_2-1.py
@@ -0,0 +1,5 @@
+# Author: ATN 9/28/21
+
+number_input = input("Please enter a number: ")
+if (int(number_input) % 2) == 0:
+    print("Your number " + number_input + " is even.")
---
+++ lab_2-2.py
@@ -0,0 +1,7 @@
+# Author: ATN 9/28/21
+
+number_input = input("Please enter a number: ")
+if (int(number_input) % 2) == 0:
+    print("Your number " + number_input + " is even.")
+else:
+    print("Your number " + number_input + " is odd.")
