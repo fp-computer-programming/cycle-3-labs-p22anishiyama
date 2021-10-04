ba1725b02706313f3da8b82300fedaff59e8f753
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-10-04 09:56:15 -0400

    add lab_5-1.py

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# Author: ATN 10/4/21
+
+# NameError
+# 1. int(a)
+
+# ValueError
+# 2. int('a')
+
+# TypeError
+# 3. 'a' + 2
+
+# ModuleNotFoundError
+# 4. import date
+
+# SyntaxError
+# 5. print("I'm a happy camper!)
